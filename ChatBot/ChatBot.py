440ece22d8eded7b9b73f1d6fdf2b49408b66654
Bishal Mondal <89398209+BishalMondal07@users.noreply.github.com>
2023-04-01 10:11:03 +0530

    Update ChatBot.py

--- ChatBot/ChatBot.py
+++ ChatBot/ChatBot.py
@@ -1,62 +1,63 @@
 import re
+import random
+from collections import defaultdict
+
 import long_responses as long
 
+# Define a dictionary to store responses
+responses = defaultdict(list)
 
-def message_probability(user_message, recognised_words, single_response=False, required_words=[]):
-    message_certainty = 0
-    has_required_words = True
+# Define a function to add responses to the dictionary
+def add_response(user_input, bot_response, probability=1):
+    responses[user_input].append((bot_response, probability))
 
-   
-    for word in user_message:
-        if word in recognised_words:
-            message_certainty += 1
+# Define some standard responses
+standard_responses = {
+    'hello': ['Hello!', 'Hi there!', 'Hi!'],
+    'how are you': ['I\'m doing well, thank you!', 'I\'m good. How are you?', 'I\'m great!'],
+    'goodbye': ['Goodbye!', 'See you later!', 'Bye!'],
+    'thank you': ['You\'re welcome!', 'No problem!', 'Anytime!']
+}
 
-    
-    percentage = float(message_certainty) / float(len(recognised_words))
+# Add the standard responses to the dictionary
+for user_input, bot_responses in standard_responses.items():
+    for bot_response in bot_responses:
+        add_response(user_input, bot_response)
 
-  
-    for word in required_words:
-        if word not in user_message:
-            has_required_words = False
-            break
+# Add some additional responses
+add_response('what is your name', 'My name is Chatbot!')
+add_response('how old are you', 'I\'m just a computer program, so I don\'t have an age!')
+add_response('what can you do', 'I can answer questions and have conversations with you!')
 
-    if has_required_words or single_response:
-        return int(percentage * 100)
-    else:
-        return 0
-
-
-def check_all_messages(message):
-    highest_prob_list = {}
-
-    
-    def response(bot_response, list_of_words, single_response=False, required_words=[]):
-        nonlocal highest_prob_list
-        highest_prob_list[bot_response] = message_probability(message, list_of_words, single_response, required_words)
-
-    
-    response('Hello!', ['hello', 'hi', 'hey', 'sup', 'heyo'], single_response=True)
-    response('See you!', ['bye', 'goodbye'], single_response=True)
-    response('I\'m doing fine, and you?', ['how', 'are', 'you', 'doing'], required_words=['how'])
-    response('You\'re welcome!', ['thank', 'thanks'], single_response=True)
-    response('Thank you!', ['i', 'love', 'code', 'palace'], required_words=['code', 'palace'])
-
-    
-    response(long.R_ADVICE, ['give', 'advice'], required_words=['advice'])
-    response(long.R_EATING, ['what', 'you', 'eat'], required_words=['you', 'eat'])
-
-    best_match = max(highest_prob_list, key=highest_prob_list.get)
-    
-    return long.unknown() if highest_prob_list[best_match] < 1 else best_match
-
-
-
+# Define a function to get a response from the bot
 def get_response(user_input):
-    split_message = re.split(r'\s+|[,;?!.-]\s*', user_input.lower())
-    response = check_all_messages(split_message)
-    return response
+    # Convert the user input to lowercase and split it into words
+    words = re.split(r'\s+|[,;?!.-]\s*', user_input.lower())
+    # Initialize a dictionary to store the probabilities of each response
+    probabilities = defaultdict(float)
+    # Loop through the responses in the dictionary
+    for user_response, bot_responses in responses.items():
+        # Check if the user input contains the response
+        if user_response in user_input:
+            # Loop through the possible bot responses for the user input
+            for bot_response, probability in bot_responses:
+                # Calculate the probability of the bot response based on the words in the user input
+                for word in words:
+                    if word in bot_response.lower().split():
+                        probabilities[bot_response] += probability / len(bot_responses)
+                        break
+    # If there are no matching responses, use a fallback response
+    if not probabilities:
+        return random.choice(long.unknown())
+    # Otherwise, return the response with the highest probability
+    return max(probabilities, key=probabilities.get)
 
-
-
+# Start the chatbot
+print('Chatbot: Hi! I\'m Chatbot. What can I help you with today?')
 while True:
-    print('Bot: ' + get_response(input('You: ')))
+    user_input = input('You: ')
+    if user_input.lower() in ['exit', 'quit', 'goodbye']:
+        print('Chatbot: Goodbye!')
+        break
+    bot_response = get_response(user_input)
+    print('Chatbot:', bot_response)
